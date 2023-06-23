5187001cad53056dbff6eb625370435a5d6caa33
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-06-23 03:17:59 +0000

    Betty Correction

--- get_opcode_function.c
+++ get_opcode_function.c
@@ -8,24 +8,24 @@
  */
 void (*get_op_function(char *cmd))(stack_t **, unsigned int)
 {
-        instruction_t operations[] = {
-                {"push", op_push},
-                {"pop", op_pop},
-                {"pall", op_pall},
-                {"pint", op_pint},
-                {"swap", op_swap},
-                {"add", op_add},
-                {"nop", op_nop},
-                {NULL, op_nop}
-        };
-        int i = 0;
+	instruction_t operations[] = {
+		{"push", op_push},
+		{"pop", op_pop},
+		{"pall", op_pall},
+		{"pint", op_pint},
+		{"swap", op_swap},
+		{"add", op_add},
+		{"nop", op_nop},
+		{NULL, op_nop}
+	};
+	int i = 0;
 
-        for ( ; i < 8; i++)
-        {
-                if (cmd == NULL)
-                        return (operations[7].f);
-                if (strcmp(operations[i].opcode, cmd) == 0)
-                        return (operations[i].f);
-        }
-        return (operations[7].f);
+	for ( ; i < 8; i++)
+	{
+		if (cmd == NULL)
+			return (operations[7].f);
+		if (strcmp(operations[i].opcode, cmd) == 0)
+			return (operations[i].f);
+	}
+	return (operations[7].f);
 }
--- more_opcode_functions.c
+++ more_opcode_functions.c
@@ -8,12 +8,12 @@
  */
 void op_add(stack_t **stack, unsigned int line_number)
 {
-        if (stack_size(stack) < 2)
-        {
-                fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-                exit(EXIT_FAILURE);
-        }
-        add(stack);
+	if (stack_size(stack) < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	add(stack);
 }
 
 /**
@@ -23,6 +23,6 @@ void op_add(stack_t **stack, unsigned int line_number)
  *
  */
 void op_nop(stack_t **stack __attribute__((unused)),
-                unsigned int line_number __attribute__((unused)))
+		unsigned int line_number __attribute__((unused)))
 {
 }
