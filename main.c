39f2e68c335756715f8c937595e13c003fd28df2
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-06-23 02:02:39 +0000

    main.c created

---
+++ main.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+/**
+ * main- entry code
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	FILE *fstream;
+	char *line = NULL, *cmd = NULL;
+	size_t len = 0, line_number = 0;
+	void (*func)(stack_t **, unsigned int);
+	stack_t *head = NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	fstream = fopen(argv[1], "r");
+	if (fstream == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&line, &len, fstream) != -1)
+	{
+		line_number++;
+		cmd = strtok(line, " \n\t");
+		extern_param = strtok(NULL, " \n\t");
+		func = get_op_function(cmd);
+		func(&head, line_number);
+	}
+	free(line);
+	free_stack(&head);
+	fclose(fstream);
+	return (0);
+}
