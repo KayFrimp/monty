db287062d76cb483d37bb7028e7a3243dc64fea7
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-06-25 16:06:21 -0700

    more function declarations

--- more_opcode_functions.c
+++ more_opcode_functions.c
@@ -8,12 +8,12 @@
  */
 void op_add(stack_t **stack, unsigned int line_number)
 {
-	if (stack_size(stack) < 2)
-	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	add(stack);
+        if (stack_size(stack) < 2)
+        {
+                fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+        _add(stack);
 }
 
 /**
@@ -23,6 +23,64 @@ void op_add(stack_t **stack, unsigned int line_number)
  *
  */
 void op_nop(stack_t **stack __attribute__((unused)),
-		unsigned int line_number __attribute__((unused)))
+                unsigned int line_number __attribute__((unused)))
 {
+}
+
+/**
+ * op_sub - subtracts the top two elements of the stack
+ * @stack: pointer to the head of the stack
+ * @line_number: operation line number
+ *
+ */
+void op_sub(stack_t **stack, unsigned int line_number)
+{
+        if (stack_size(stack) < 2)
+        {
+                fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+        _sub(stack);
+}
+
+/**
+ * op_div - divides the top two elements of the stack (top 2nd / top 1st)
+ * @stack: pointer to the head of the stack
+ * @line_number: operation line number
+ *
+ */
+void op_div(stack_t **stack, unsigned int line_number)
+{
+        if (stack_size(stack) < 2)
+        {
+                fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+        if (top(stack) == 0)
+        {
+                fprintf(stderr, "L%d: division by zero\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+        _div(stack);
+}
+
+/**
+ * op_mod - modulos the top two elements of the stack (top 2nd / top 1st)
+ * @stack: pointer to the head of the stack
+ * @line_number: operation line number
+ *
+ */
+void op_mod(stack_t **stack, unsigned int line_number)
+{
+        if (stack_size(stack) < 2)
+        {
+                fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+        if (top(stack) == 0)
+        {
+                fprintf(stderr, "L%d: division by zero\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+        _mod(stack);
 }
