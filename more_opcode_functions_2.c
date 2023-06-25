e26229f967a1d466e0144695379d94ab040af68b
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-06-25 15:41:58 -0700

    More opcode functions

---
+++ more_opcode_functions_2.c
@@ -0,0 +1,49 @@
+#include "monty.h"
+
+/**
+ * op_mul - multiply the top two elements of the stack
+ * @stack: pointer to the head of the stack
+ * @line_number: operation line number
+ *
+ */
+void op_mul(stack_t **stack, unsigned int line_number)
+{
+	if (stack_size(stack) < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	_mul(stack);
+}
+
+/**
+ * op_pchar - print the char at the top of the stack
+ * @stack: pointer to the head of the stack
+ * @line_number: operation line number
+ *
+ */
+void op_pchar(stack_t **stack, unsigned int line_number)
+{
+	if (is_empty(stack) == 1)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n < 32 || (*stack)->n > 127)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	pchar(stack);
+}
+
+/**
+ * op_pstr - prints the string starting at the top of the stack
+ * @stack: pointer to the head of the stack
+ * @line_number: operation line number
+ *
+ */
+void op_pstr(stack_t **stack, unsigned int line_number)
+{
+	pstr(stack);
+}
